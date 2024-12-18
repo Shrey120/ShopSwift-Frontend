a8165ffccfdfbce8bfbbcdc8666219ba85a82ebb
shrey gupta <shreygupta12028@gmail.com>
2024-12-18 20:32:11 +0530

    price

--- src/components/ManyItems.js
+++ src/components/ManyItems.js
@@ -16,11 +16,11 @@ export default function ManyItems() {
     onCheckOut,
   } = useAppContext();
 
-  let shippingFee = 7000;
+  let shippingFee = 0;
   return (
     <>
       {" "}
-      <div className="cart-heading-grid-five-column head">
+      <div className='cart-heading-grid-five-column head'>
         <p>Item</p>
         <p>Price</p>
         <p>Quantity</p>
@@ -28,29 +28,29 @@ export default function ManyItems() {
         <p>Remove</p>
       </div>
       <hr />
-      <div className="cart-item">
+      <div className='cart-item'>
         {cartItems &&
           cartItems.map((curElem) => {
             return (
               <div
-                className="cart-heading-grid-five-column "
+                className='cart-heading-grid-five-column '
                 key={curElem.id}>
-                <div className="cart-image--name">
+                <div className='cart-image--name'>
                   <div>
                     <figure>
                       <img
-                        class="cart-image"
+                        class='cart-image'
                         src={curElem.image.url}
-                        alt="Image Missing"
+                        alt='Image Missing'
                       />
                     </figure>
                   </div>
-                  <div className="name-color">
+                  <div className='name-color'>
                     <p>{curElem.name}</p>
                     <p>
                       color:
                       <button
-                        className="color-div"
+                        className='color-div'
                         style={{
                           backgroundColor: curElem.color,
                         }}
@@ -59,31 +59,31 @@ export default function ManyItems() {
                   </div>
                 </div>
                 {/* price   */}
-                <div className="cart-item-property price-c">
-                  <p class="currency">{formatCurrency(curElem.price)}</p>
+                <div className='cart-item-property price-c'>
+                  <p class='currency'>{formatCurrency(curElem.price)}</p>
                 </div>
 
                 {/* Quantity  */}
-                <div className="cart-item-property quant-c">
+                <div className='cart-item-property quant-c'>
                   <button
                     onClick={() => increamentOnCart(curElem.stock, curElem.id)}
-                    className="card-quantity-add-sub">
+                    className='card-quantity-add-sub'>
                     +
                   </button>
                   <input
-                    className="card-quantity-amount"
+                    className='card-quantity-amount'
                     placeholder={curElem.quantity}
                   />
                   <button
                     onClick={() => decreamentOnCart(curElem.id)}
-                    className="card-quantity-add-sub">
+                    className='card-quantity-add-sub'>
                     -
                   </button>
                 </div>
 
                 {/* //Subtotal */}
-                <div className="cart-item-property total-c">
-                  <p class="currency">
+                <div className='cart-item-property total-c'>
+                  <p class='currency'>
                     {formatCurrency(curElem.price * curElem.quantity)}
                   </p>
                 </div>
@@ -92,7 +92,7 @@ export default function ManyItems() {
                 <div>
                   <FaTrash
                     onClick={() => deleteItem(curElem.id)}
-                    className="remove-icon"
+                    className='remove-icon'
                   />
                 </div>
 
@@ -102,35 +102,35 @@ export default function ManyItems() {
           })}
       </div>
       <hr />
-      <div className="cart-button">
-        <NavLink to="/products">
-          <button className="hireme-btn cart-btn"> continue Shopping </button>
+      <div className='cart-button'>
+        <NavLink to='/products'>
+          <button className='hireme-btn cart-btn'> continue Shopping </button>
         </NavLink>
         <button
-          className="hireme-btn cart-btn"
+          className='hireme-btn cart-btn'
           onClick={clearCart}>
           clear cart
         </button>
       </div>
-      <div className="order-total--amount">
-        <div className="order-total--subdata">
+      <div className='order-total--amount'>
+        <div className='order-total--subdata'>
           <div>
             <p>subtotal:</p>
-            <p class="currency bold">{formatCurrency(cartPrice)}</p>
+            <p class='currency bold'>{formatCurrency(cartPrice)}</p>
           </div>
           <div>
             <p>shipping fee:</p>
-            <p class="currency bold">{formatCurrency(shippingFee)}</p>
+            <p class='currency bold'>{formatCurrency(shippingFee)}</p>
           </div>
           <hr />
           <div>
             <p>order total:</p>
-            <p class="currency bold">
+            <p class='currency bold'>
               {formatCurrency(shippingFee + cartPrice)}
             </p>
           </div>
           <button
-            className="hireme-btn"
+            className='hireme-btn'
             onClick={onCheckOut}>
             Order Now
           </button>
