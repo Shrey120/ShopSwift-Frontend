12b6d0b78affaf822012f19fa906088d01c62b7e
shrey gupta <shreygupta12028@gmail.com>
2024-12-18 12:39:19 +0530

    price

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -6,23 +6,23 @@ export default function GridView() {
   const { filteredItems, formatCurrency } = useAppContext();
   return (
     <>
-      <div className="main-product-box ">
+      <div className='main-product-box '>
         {filteredItems &&
           filteredItems.map((ele) => {
             return (
               <NavLink to={`/products/${ele._id}`}>
                 <div key={ele._id}>
-                  <div className="single-product-box">
+                  <div className='single-product-box'>
                     <div>
                       <img
-                        className="img-box"
+                        className='img-box'
                         src={ele.image[0].url}
-                        alt="Image"
+                        alt='Image'
                       />
                     </div>
-                    <div className="name-price-box">
-                      <div>{ele.name}</div>
-                      <div className="currency">
+                    <div className='name-price-box'>
+                      <div>{ele.name.slice(0, 20)} ....</div>
+                      <div className='currency'>
                         {formatCurrency(ele.price)}
                       </div>
                     </div>
